7792060dd18ef75173a8fa24365a502f86c5af81
Dimitar Tarkalanov <dimitar.tarkalanov@gmail.com>
2022-11-28 21:48:31 +0200

    update Exercies

--- src/exercies/Exercises.java
+++ src/exercies/Exercises.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Exercises {
     public static void main(String[] args) {
-        printName();
+        printQuadraticEquation();
     }
 
     public static void printName() {
@@ -18,7 +18,7 @@ public class Exercises {
     }
 
     public static void printTrianglePerimeter() {
-        int a = 3;
+        int a = 1;
         int b = 4;
         int c = 5;
 
@@ -28,7 +28,7 @@ public class Exercises {
     }
 
     public static void printTriangleArea() {
-        int a = 10;
+        int a = 20;
         int b = 5;
 
         int area = (a * b) / 2;
@@ -70,5 +70,41 @@ public class Exercises {
         int area = (a * b) / 2;
 
         System.out.println("The area of the triangle is: " + area);
+    }
+
+    public static void printQuadraticEquation() {
+        // value of the constants a, b, c
+        double a = 7.2;
+        double b = 5;
+        double c = 9;
+
+        // declared the two roots
+        double root1;
+        double root2;
+
+        // determinant (b^2 - 4ac)
+        double determinant = b * b - 4 * a * c;
+
+        System.out.println("Determinant = " + determinant);
+
+        //determinant > 0
+        root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+        root2 = (-b - Math.sqrt(determinant)) / (2 * a);
+        System.out.println("\nDeterminant > 0");
+        System.out.println("root1 = " + root1);
+        System.out.println("root2 = " + root2);
+
+
+        //determinant == 0
+        root1 = root2 = -b / (2 * a);
+        System.out.println("\nDeterminant == 0");
+        System.out.println("root1 = root2 = " + root1);
+
+        //determinant < 0
+        double real = -b / (2 * a);
+        double imaginary = Math.sqrt(-determinant) / (2 * a);
+        System.out.println("\nDeterminant < 0");
+        System.out.format("root1 = %.2f+%.2fi", real, imaginary);
+        System.out.format("\nroot2 = %.2f-%.2fi", real, imaginary);
     }
 }
