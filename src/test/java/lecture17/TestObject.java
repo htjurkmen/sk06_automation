841e5dd6e066723f506633f25ef4a404a832008c
Dimitar Tarkalanov <dimitar.tarkalanov@gmail.com>
2023-02-05 15:21:40 +0200

    lecture-17 add take screenshot on test failure

--- src/test/java/lecture17/TestObject.java
+++ src/test/java/lecture17/TestObject.java
@@ -2,10 +2,13 @@ package lecture17;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -18,7 +21,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TestObject {
-    public static final String DOWNLOAD_DIR = "src\\test\\resources\\download\\";
+    public static final String TEST_RESOURCES_DIR = "src\\test\\resources\\";
+    public static final String DOWNLOAD_DIR = TEST_RESOURCES_DIR.concat("download\\");
+    public static final String SCREENSHOTS_DIR = TEST_RESOURCES_DIR.concat("screenshots\\");
     private WebDriver driver;
 
     @BeforeSuite
@@ -37,10 +42,9 @@ public class TestObject {
     }
 
     @AfterMethod
-    protected final void tearDownTest() {
-        if (this.driver != null) {
-            this.driver.quit();
-        }
+    protected final void tearDownTest(ITestResult testResult) {
+        takeScreenshot(testResult);
+        quitDriver();
     }
 
     @AfterSuite
@@ -55,6 +59,26 @@ public class TestObject {
             System.out.println("Downloaded files are deleted!");
         } else {
             System.out.println("Downloaded files are NOT deleted!");
+        }
+    }
+
+    private void takeScreenshot(ITestResult testResult) {
+        if (ITestResult.FAILURE == testResult.getStatus()) {
+            try {
+                TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+                File screenshot = takesScreenshot.getScreenshotAs(OutputType.FILE);
+                String testName = testResult.getName();
+                FileUtils.copyFile(screenshot, new File(SCREENSHOTS_DIR.concat(testName).concat(".jpg")));
+            } catch (IOException e) {
+                System.out.println("Unable to create a screenshot file: " + e.getMessage());
+            }
+        }
+
+    }
+
+    private void quitDriver() {
+        if (this.driver != null) {
+            this.driver.quit();
         }
     }
 
